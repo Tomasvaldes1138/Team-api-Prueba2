f562815ccf13eed9436723ea63570af91b0d7a4f
Daira0811 <d.acuna05@ufromail.cl>
2023-11-06 20:04:44 -0300

    FIX: Se refactoriza código duplicado en getOne.test.js

--- test/business-logic/users/getOne.test.js
+++ test/business-logic/users/getOne.test.js
@@ -7,13 +7,18 @@ import mongoose from 'mongoose';
 jest.mock('../../../src/models/user/user.model');
 
 describe('Business logic: User: Get one user', () => { 
+    const user = {
+        name: 'usuario',
+        email: 'correo3@gmail.com',
+        password: '123',
+        isAdmin: false 
+    }
 
     afterEach(async () => {
         jest.resetAllMocks();
     });
 
     it('Should get the user', async () => {
-        const user = { name: 'usuario', email: 'correo3@gmail.com', password: '123', isAdmin: false };
         const query = new mongoose.Query();
         query.populate = jest.fn().mockReturnThis(); 
         query.select= jest.fn().mockReturnThis({ email: user.email });
@@ -22,16 +27,13 @@ describe('Business logic: User: Get one user', () => {
 
         const resp = await getOne({ query: { email: user.email }, select: ['name', 'email'], populate: ['isAdmin']});
         expect(resp).not.toBeNull();
-        expect 
         expect(resp.name).toEqual(user.name);
         expect(resp.email).toEqual(user.email);
         expect(resp.isAdmin).toEqual(user.isAdmin);
     })
 
     it('Should get null if the user doesnt exist', async () => {
-        const user = { name: 'usuario', email: 'correo3@gmail.com', password: '123', isAdmin: false };
         UserModel.findOne.mockResolvedValue(null);
-
         const resp = await getOne({ query: { email: user.email }});
         expect(resp).toBeNull();
     })
