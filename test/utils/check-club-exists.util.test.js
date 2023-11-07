dafdade80c1fb6233fde338e859ea6ff19c6edf8
Daira0811 <d.acuna05@ufromail.cl>
2023-11-07 20:00:02 -0300

    FIX: Se valida camino de error en el archivo check-club-exists.util.test.js

--- test/utils/check-club-exists.util.test.js
+++ test/utils/check-club-exists.util.test.js
@@ -1,4 +1,5 @@
 import ClubLogic from '../../src/business-logic/club';
+import HTTPError from '../../src/errors/http.error';
 import checkClubExists from '../../src/utils/check-club-exists.util';
 
 jest.mock('../../src/business-logic/club');
@@ -7,7 +8,7 @@ describe('Utils: util: check club exists', () => {
   let mockErrorObject;
 
   beforeEach(() => {
-    mockErrorObject = new Error('Club not found');
+    mockErrorObject = new HTTPError ({ name: 'error', message: 'some-error', code: 500 });
   });
 
   afterEach(() => {
@@ -21,7 +22,13 @@ describe('Utils: util: check club exists', () => {
   });
 
   it('should throw an error when the club does not exist', async () => {
-    ClubLogic.get.mockResolvedValue(null);
-    await expect(checkClubExists({ clubId: 'club-id', errorObject: mockErrorObject })).rejects.toThrow(mockErrorObject);
+    try {
+      ClubLogic.get.mockResolvedValue(null);
+      await expect(checkClubExists({ clubId: 'club-id', errorObject: mockErrorObject })).rejects.toThrow();
+    } catch (error) { 
+      expect(error.name).toEqual('error');
+      expect(error.message).toEqual('some-error');
+      expect(error.code).toEqual(500);
+    }
   });
 });
